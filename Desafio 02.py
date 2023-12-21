77c93e5276a79e59b170e3c617e70c13b3915b68
enghonorios <154471089+enghonorios@users.noreply.github.com>
2023-12-21 11:20:24 -0300

    Python desafio 02 iniciantes

---
+++ Desafio 02.py
@@ -0,0 +1,8 @@
+print (' ====== Desafio 02 ===== ')
+
+dia= input ('Que dia você nasceu?')
+mês= input ('Mês você nasceu?')
+ano= input ('Qual ano você nasceu?')
+
+print ('Você nasceu no dia', dia, 'de', mês, 'no ano de', ano, 'correto?')
+
